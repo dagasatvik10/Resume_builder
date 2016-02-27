7fdd10218ae7f18cae954ff67f00bb6a48f2df1e
Ekta3012 <ekta30121995@gmail.com>
2016-02-27 18:30:47 +0530

    Form Views:Basic_info,education,work_experience,objective,skills,personal_details,projects without styling created

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,14 +1,11 @@
 <?php
 
-// Route::get('/',function(){
+Route::get('users/Basic_info','UserController@index');
+Route::get('users/education','EducationController@index');
+Route::get('users/work_experience','WorkExperienceController@index');
+Route::get('users/personal_details','PersonalDetailsController@index');
+Route::get('users/skill','SkillsController@index');
+Route::get('users/objective','ObjectiveController@index');
+Route::get('users/project','ProjectController@index');
 
-// 	return Form::text('usr');
-// });
-Route::group(['middleware' => ['web']], function()
-{
-	Route::get('auth/ln', 'Auth\AuthController@redirectToLinkedin');
-	Route::get('auth/ln/callback', 'Auth\AuthController@handleLinkedinCallback');
-	Route::get('auth/fb', 'Auth\AuthController@redirectToFacebook');
-	Route::get('auth/fb/callback', 'Auth\AuthController@handleFacebookCallback');
-});
 
