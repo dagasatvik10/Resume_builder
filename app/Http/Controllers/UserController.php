dab48cb3f56eb214016eecd3a1781c4371a4f675
Satvik <dagasatvik10@gmail.com>
2016-03-17 21:58:50 +0530

    homepage done....working via route

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -30,9 +30,10 @@ class UserController extends Controller
 
         return view('resume');
     }
+
     public function homepage()
     {
-        return view('homepage');
+        return view('vendor.homepage');
     }
 
 
---
+++ resources/views/vendor/homepage.blade.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Resume Builder | Software Incubator</title>
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+	<link rel="stylesheet" type="text/css" href="css/stylesheet.css">
+</head>
+<body>
+	<!--fixed navigation-->
+	<nav class="navbar navbar-fixed-top" style="font-size: 20px; color: #fff;">
+		<div class="container-fluid" >
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#mynavbar" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            	</button>
+				<a class="navbar-brand" href="#">Resume Builder</a>
+			</div>
+			<div class="collapse navbar-collapse" id="mynavbar">
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="#">Customer Reviews</a></li>
+					<li><a href="#"> Sample Designs</a></li>
+					<li><a href="#"> Contact Us</a></li>
+					<li><a href="#">LogIn</a></li>
+				</ul>
+			</div>				
+		</div>			
+	</nav>
+	<div id="content-land">
+		<div style="font-size:45px; padding-top: 200px;">Create a standout resume in minutes.</div>
+		<div style="font-size:35px; padding-top: 20px; padding-bottom: 20px;">Easily create professional resumes.</div>
+		<button type="button" class="btn btn-default" id="create_button">Create Resume Now</button>
+	</div>
+	<div id="download">
+		<span>Downloaded 0</span> <span>Saved 0</span>
+	</div>
+	<div class="container-fluid" id="why_rb">
+		<h2 style="text-align: center;">Why Resume Builder</h2>
+			Resume Builder makes it fast and easy to create the powerful, professional resume hiring managers are looking for.<br>
+			Our Resume Builder lets you create a resume in minutes!
+	</div>
+	<div class="container-fluid" id="how_itworks">
+		<h1 style="text-align: center;">How It <span style="color: #15b8db">Works</span></h1>
+		<div class="container step">
+			<div style="float: left;">
+				<img src="img/macpro.png" class="img-responsive">
+			</div>
+			<div style="float: right; font-size: 25px; width: 50%;">
+				&#9312; &nbsp &nbsp Build a resume with the help of few simple steps.
+				
+			</div>
+		</div><hr><br>
+		<div class="container step">
+			<div style="float:left; font-size: 25px; width: 50%;">
+				&#9313; &nbsp &nbsp Choose a template, of your choice.Make your resume of your choice. 
+			</div>
+			<div style="float: right;">
+				<img src="img/templates.png" class="img-responsive">
+			</div>
+		</div><hr>
+		<div class="container step">
+			<div style="float: left;">
+				<img src="img/download.png" class="img-responsive">
+			</div>
+			<div style="float: right; font-size: 25px; width: 50%;">
+				&#9314; &nbsp &nbsp Download Your resume in pdf or word document format.
+			</div>
+		</div><hr>
+		<div class="container step">
+			<div style="float: left; font-size: 25px; width: 50%;">
+				&#9315; &nbsp &nbsp Save your resume for future reference. You can edit/ view your resume in future. By creating an account in Resume Builder.
+			</div>
+			<div style="float: right;">
+				<img src="img/login.png" class="img-responsive">
+			</div>
+		</div><hr>
+	</div>
+<div class="container">
+  <br>
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+      <li data-target="#myCarousel" data-slide-to="3"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner" role="listbox">
+      <div class="item active">
+        <img src="img/template1.jpg" alt="Chania" width="460" height="345">
+      </div>
+
+      <div class="item">
+        <img src="img/template2.png" alt="Chania" width="460" height="345">
+      </div>
+    
+      <div class="item">
+        <img src="img/template3.jpg" alt="Flower" width="460" height="345">
+      </div>
+
+      <div class="item">
+        <img src="img/template4.png" alt="Flower" width="460" height="345">
+      </div>
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</div>
+
+
+
+	<div class="container-fluid" id="features">
+		<h1 style="text-align: center; margin-bottom: 80px;">Features</h1>
+		<ul style="list-style: none;">
+			<li>Easy to create with social login</li>
+			<li>Provides different design templates</li>
+			<li>You can Save and download your resume for future.</li>
+			<li>You can Edit your resume. Change the templates</li>
+		</ul>
+	</div>
+	<div class="container" id="contact">
+		<h1 style="text-align: center; color: #15b8db; margin-bottom: 30px; ">Contact Us</h1>
+		Thanks for your interest in Resume Builder. If you have a question regarding your Resume Builder, please visit our support forums. To get in touch, use the contacts listed below.
+		<ul>
+			<li>silive.in</li>
+			<li>www.facebook.com/si</li>
+		</ul>
+	</div>
+	<footer class="container-fluid panel-footer">
+		ResumeBuilder-2016 &copy; @ Software Incubator.
+		<ul style="list-style: none; display: inline;">
+			<li style="display: inline;"><img src="img/fb.png" class="f_img"></li>
+			<li style="display: inline;"><img src="img/twitter.png"class="f_img"></li>
+			<li style="display: inline;"><img src="img/google.png" class="f_img"></li>
+		</ul>
+	</footer>
+</body>
+</html>
