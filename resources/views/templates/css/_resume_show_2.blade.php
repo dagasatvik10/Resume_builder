2518c2f3ab1b6e6c675a23cf1dccc0084254d85a
Ekta3012 <ekta30121995@gmail.com>
2016-04-17 03:08:19 +0530

    second template

---
+++ resources/views/templates/css/_resume_show_2.blade.php
@@ -0,0 +1,13 @@
+<style type="text/css">
+	#personal_details
+	{
+		text-align: right;
+	}
+	#name{
+		text-align: left;
+		background-color: #8b9dc3;
+		padding: 5px;
+		color: #fff;
+	}
+
+</style>
