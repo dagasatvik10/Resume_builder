b88e07e1d245d7da0495ccc10999b85a2ea33c60
Ekta Tiwari <Ekta Tiwari>
2016-09-24 16:32:21 +0530

    arrows added on homepage

--- config/Wkhtml2pdf.php
+++ config/Wkhtml2pdf.php
@@ -1,8 +1,9 @@
 <?php
 
 return array(
+
     'debug' => false,
     'binpath' => 'lib/',
-	'binfile' => 'wkhtmltopdf-0.12.1-OS-X.i386',
+	'binfile' => 'wkhtmltopdf-amd64',
 	'output_mode' => 'I'
 );
